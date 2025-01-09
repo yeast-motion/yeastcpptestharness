27378a8ff27d5461c65d21d8e60ef873bead3de8
Michael Todd <eltodd@gmail.com>
2025-01-08 21:56:37 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# yeastcpptestharness
+# yeastcpppathplannertrajectoryfollower
---
+++ src/yeastcpptestharness.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <memory>
+
+#include <yeastcppwpilibdrivecontroller/wpilibdrivecontroller.hpp>
+
+// cma
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Runnin" << std::endl;
+}
