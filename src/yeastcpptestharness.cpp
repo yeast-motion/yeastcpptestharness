b7411b389a28cf87a2bd1095453bbdd3380b081b
Michael Todd <eltodd@gmail.com>
2025-01-09 20:24:53 -0500

    Cont updates

--- src/yeastcpptestharness.cpp
+++ src/yeastcpptestharness.cpp
@@ -3,13 +3,15 @@
 
 #include <yeastcppwpilibdrivecontroller/wpilibdrivecontroller.hpp>
 #include <yeastcppwpilibodometryprovider/wpilibodometryprovider.hpp>
+#include <yeastcpppathplannertrajectoryfollower/pathplannertrajectoryfollower.hpp>
 
 
 std::shared_ptr<yeast_motion::WPILibDriveController> controller;
 std::shared_ptr<yeast_motion::WPILibOdometryProvider> odometry;
+std::shared_ptr<yeast_motion::PathPlannerTrajectoryFollower> pathfollower;
 
 float sim_time = 0;
-float dt = 0.1;
+float dt = 0.01;
 
 yeast_motion::OdometrySample pose;
 
@@ -59,30 +61,9 @@ void simulate()
     command.velocity_valid = true;
     command.velocity.x = 3;
 
-    std::cout << "ChassisCommand: " << command.to_json() << std::endl;
-
     controller->update_motor_status(module_statuses);
     controller->drive(command);
     auto module_commands = controller->get_command();
-
-    int print_line = 0;
-
-    for (auto command : module_commands)
-    {
-        std::cout << "Command:" << print_line << command.to_json() << std::endl;
-        print_line ++;
-    }
-
-    print_line = 0;
-    for (int i = 0; i < module_statuses.size(); i++)
-    {
-        module_statuses[i].position += module_commands[i].speed * dt;
-        module_statuses[i].speed = module_commands[i].speed;
-        module_statuses[i].theta = module_commands[i].theta;
-        std::cout << "Status:" << print_line << module_statuses[i].to_json() << std::endl;
-        print_line ++;
-    }
-
 
     yeast_motion::Rotation2D drive_theta;
     drive_theta.theta = 0;
@@ -90,22 +71,34 @@ void simulate()
     pose = odometry->update(module_statuses, drive_theta);
 }
 
-int main(int argc, char *argv[])
+void print_time (float sim_time)
 {
-    init();
-    std::cout << "Runnin" << std::endl;
-
-    while (sim_time < 1.0)
+    static int whole_time = 0;
+    if ((int) sim_time > whole_time)
     {
         std::cout << "----------------------------------------" << std::endl;
-        std::cout << "Step: " << sim_time << std::endl;
+        std::cout << "Seconds: " << whole_time << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+        whole_time = (int) sim_time;
+    }
+}
 
+int main(int argc, char *argv[])
+{
+    std::cout << "----------------------------------------" << std::endl;
+    init();
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "Runnin" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+
+    while (sim_time < 10.0)
+    {
+        print_time(sim_time);
         simulate();
         sim_time += dt;
-        std::cout << "----------------------------------------" << std::endl;
-
     }
 
+    print_time(sim_time);
     std::cout << "----------------------------------------" << std::endl;
     std::cout << "End Pose: " << pose.to_json().dump(2) << std::endl;
     std::cout << "----------------------------------------" << std::endl;
