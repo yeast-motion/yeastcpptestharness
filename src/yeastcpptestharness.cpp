ca941fbf34124fa6c70a5291a5e03fe138500158
Michael Todd <eltodd@gmail.com>
2025-01-10 14:30:25 -0500

    Cont update

--- src/yeastcpptestharness.cpp
+++ src/yeastcpptestharness.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <memory>
 #include <fstream>
+#include <thread>
+#include <chrono>
 
 #include <yeastcppwpilibdrivecontroller/wpilibdrivecontroller.hpp>
 #include <yeastcppwpilibodometryprovider/wpilibodometryprovider.hpp>
 #include <yeastcpppathplannertrajectoryfollower/pathplannertrajectoryfollower.hpp>
-
-
 
 std::shared_ptr<yeast_motion::WPILibDriveController> controller;
 std::shared_ptr<yeast_motion::WPILibOdometryProvider> odometry;
@@ -15,7 +15,7 @@ std::shared_ptr<yeast_motion::PathPlannerTrajectoryFollower> pathfollower;
 float sim_time = 0;
 float dt = 0.01;
 
-yeast_motion::OdometrySample pose;
+yeast_motion::MotionState motion_state;
 
 void init()
 {
@@ -51,17 +51,17 @@ void init()
     {
         pathfollower.reset (new yeast_motion::PathPlannerTrajectoryFollower());
 
-        // {
-        //     std::ifstream f("settings.json");
-        //     nlohmann::json config = nlohmann::json::parse(f);
-        //     pathfollower->set_config(config);
-        // }
+        {
+            std::ifstream f("settings.json");
+            nlohmann::json config = nlohmann::json::parse(f);
+            pathfollower->set_config(config);
+        }
 
-        // {
-        //     std::ifstream f("Path1.json");
-        //     nlohmann::json path = nlohmann::json::parse(f);
-        //     pathfollower->begin(path);
-        // }
+        {
+            std::ifstream f("Path1.json");
+            nlohmann::json path = nlohmann::json::parse(f);
+            pathfollower->begin(path);
+        }
     }
 }
 
@@ -81,18 +81,31 @@ void simulate()
         }
     }
 
-    yeast_motion::MotionCommand command;
-    command.velocity_valid = true;
-    command.velocity.x = 3;
+    auto command = pathfollower->follow(motion_state);
+    auto follower_status = pathfollower->status();
+
+    if (follower_status.finished)
+    {
+        command.velocity.x = 0;
+        command.velocity.y = 0;
+        command.velocity.omega = 0;
+    }
 
     controller->update_motor_status(module_statuses);
     controller->drive(command);
     auto module_commands = controller->get_command();
 
-    yeast_motion::Rotation2D drive_theta;
-    drive_theta.theta = 0;
+    for (size_t i = 0; i < 4; i++)
+    {
+        module_statuses[i].speed = module_commands[i].speed;
+        module_statuses[i].position += module_commands[i].speed * dt;
+        module_statuses[i].theta = module_commands[i].theta;
+    }
 
-    pose = odometry->update(module_statuses, drive_theta);
+    yeast_motion::Rotation2D drive_theta;
+    drive_theta.theta = motion_state.measurement.pose.rotation.theta + command.velocity.omega * dt;
+
+    motion_state.measurement = odometry->update(module_statuses, drive_theta);
 }
 
 void print_time (float sim_time)
@@ -102,28 +115,38 @@ void print_time (float sim_time)
     {
         std::cout << "----------------------------------------" << std::endl;
         std::cout << "Seconds: " << whole_time << std::endl;
+        std::cout << "Pose: " << motion_state.measurement.pose.to_json().dump(2) << std::endl;
         std::cout << "----------------------------------------" << std::endl;
         whole_time = (int) sim_time;
     }
+}
+
+void cleanup()
+{
+    pathfollower.reset();
+    controller.reset();
+    odometry.reset();
 }
 
 int main(int argc, char *argv[])
 {
     init();
     std::cout << "----------------------------------------" << std::endl;
-    std::cout << "Runnin" << std::endl;
+    std::cout << "Running" << std::endl;
     std::cout << "----------------------------------------" << std::endl;
 
     while (sim_time < 10.0)
     {
-        print_time(sim_time);
+        print_time(sim_time + 1);
         simulate();
         sim_time += dt;
+        std::this_thread::sleep_for(std::chrono::milliseconds((int64_t) (dt * 1000)));
     }
 
     print_time(sim_time);
     std::cout << "----------------------------------------" << std::endl;
-    std::cout << "End Pose: " << pose.to_json().dump(2) << std::endl;
+    std::cout << "End Pose: " << motion_state.measurement.pose.to_json().dump(2) << std::endl;
     std::cout << "----------------------------------------" << std::endl;
 
+    cleanup();
 }
